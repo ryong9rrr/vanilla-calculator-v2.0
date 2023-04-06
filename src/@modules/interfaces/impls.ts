7cf4ba6dd20b43b9436ceb2321f441e36299bad2
yong <tkddbs1644@gmail.com>
2023-04-06 18:26:20 +0900

    config: Component core modules 추가

--- src/@modules/interfaces/impls.ts
+++ src/@modules/interfaces/impls.ts
@@ -1,3 +1,8 @@
 export interface CalculatorServiceImpl {
   calculate: (exp: string) => number
 }
+
+export interface ProviderImpl {
+  subscribe: (fn: () => void) => void
+  unsubscribe: (fn: () => void) => void
+}
