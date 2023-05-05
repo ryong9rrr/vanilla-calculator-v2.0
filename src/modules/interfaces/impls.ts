841b2e013d446956307b6d94b374aba0328cf4e2
yong <tkddbs1644@gmail.com>
2023-05-05 19:00:30 +0900

    fix: 필요없는 interface 삭제, sangyoon-ui version을 1.1.8로 업데이트

--- src/modules/interfaces/impls.ts
+++ src/modules/interfaces/impls.ts
@@ -1,8 +1,3 @@
 export interface CalculatorServiceImpl {
   calculate: (exp: string) => number
 }
-
-export interface ProviderImpl {
-  subscribe: (fn: () => void) => void
-  unsubscribe: (fn: () => void) => void
-}
